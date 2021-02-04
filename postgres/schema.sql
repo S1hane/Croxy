b94b041c31ea2ae59509f529290e9e9c86587b35
Shane Lemmer <34974305+S1hane@users.noreply.github.com>
2021-02-04 13:34:43 -0700

    added docker config

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -1,3 +1,5 @@
+DROP USER IF EXISTS student;
+CREATE USER student WITH PASSWORD 'student';
 
 DROP TABLE IF EXISTS images;
 CREATE TABLE images
@@ -15,35 +17,13 @@ CREATE TABLE images
   fav boolean NULL
 );
 
--- none of these records should be inserted due to constraint voilations!
-INSERT INTO images (id, i1_4, i5_8, i9, fav) VALUES
-  (1, 10000000000000000, 7777777777777777, 7777, 'f');
+GRANT SELECT, UPDATE, INSERT ON TABLE images TO student;
 
-INSERT INTO images (id, i1_4, i5_8, i9, fav) VALUES
-  (2, -1, 7777777777777777, 7777, 'f');
-
-INSERT INTO images (id, i1_4, i5_8, i9, fav) VALUES
-  (3, 7777777777777777, 10000000000000000, 7777, 'f');
-
-INSERT INTO images (id, i1_4, i5_8, i9, fav) VALUES
-  (4, 7777777777777777, -1, 7777, 'f');
-
-INSERT INTO images (id, i1_4, i5_8, i9, fav) VALUES
-  (5, 7777777777777777, 7777777777777777, 10000, 'f');
-
-INSERT INTO images (id, i1_4, i5_8, i9, fav) VALUES
-  (6, 7777777777777777, 7777777777777777, -1, 'f');
-
-INSERT INTO images (id, i1_4, i5_8, i9, fav) VALUES
-  (-1, 7777777777777777, 7777777777777777, -1, 'f');
--- none of these records should be inserted due to constraint voilations!
-
+-- These test records are inserted above the 10M record scope of the project.
+-- Feel free to remove them. They just facilitate testing of an unseeded database.
 INSERT INTO images (id, i1_4, i5_8, i9, fav)
 VALUES
-  (9999999, 1111222233334444, 5555666677778888, 9999, 't'),
-  (9999998, 0000000000000000, 0000000000000000, 0000, 'f'),
-  (9999997, 9999999999999999, 9999999999999999, 9999, 'f');
---- you should see these 3 test records in the database
+  (10000000, 1111222233334444, 5555666677778888, 9999, 't'),
+  (10000001, 0000000000000000, 0000000000000000, 0000, 'f'),
+  (10000002, 9999999999999999, 9999999999999999, 9999, 'f');
 SELECT (i1_4, i5_8, i9, fav) FROM images;
-
-
