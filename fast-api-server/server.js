d733df38dea3cd543db71d1d9578372b3a514500
Shane Lemmer <34974305+S1hane@users.noreply.github.com>
2021-02-09 10:35:43 -0700

    updates to target db

--- fast-api-server/server.js
+++ fast-api-server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fastify = require('fastify')();
 const views = require(path.join(__dirname, 'modules', 'views.js'));
-const DATABASE_URL = 'postgres://student:student@44.192.73.22/postgres';
+const DATABASE_URL = 'postgres://student:student@3.210.184.251/postgres';
 const port = 3004;
 require('newrelic');
 
