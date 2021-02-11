466a2befaa2db532c98c2659edddfa85713fa924
Shane Lemmer <34974305+S1hane@users.noreply.github.com>
2021-02-11 11:49:54 -0700

    updated ip

--- fast-api-server/server.js
+++ fast-api-server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fastify = require('fastify')();
 const views = require(path.join(__dirname, 'modules', 'views.js'));
-const DATABASE_URL = 'postgres://student:student@172.31.83.56/postgres';
+const DATABASE_URL = 'postgres://student:student@172.31.74.177/postgres';
 const port = 3004;
 // require('newrelic');
 
